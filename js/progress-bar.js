99d5a60871ee23c8bb9a754edbd4b8c396c19af7
Александр Гвоздецкий <gvozdik0806@ukr.net>
2021-04-10 22:16:05 +0300

    adding 3features such as popup, text animation and cool navigation link

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -1,13 +1,4 @@
-// const progress = document.querySelector('.progress');
 
-// window.addEventListener('scroll', progressBar);
-
-// function progressBar(e) {
-//     let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
-//     let windowHeight = documentElement.scrollHeight - document.documentElement.clientHeight;
-//     let per = windowScroll / windowHeight * 100; 
-//     progress.style.width = per + '%';
-// }  
 function progressBar() {
     let scroll = document.body.scrollTop || document.documentElement.scrollTop;
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
